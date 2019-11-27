4bb279b27b719e3d98d47aa7974308ff2426832e
JaySebagh <jaysebagh@gmail.com>
2019-11-27 15:59:22 -0800

    Added pfp and styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './App.css';
+import Pfp from './images/pfp.jpeg';
 
 function App() {
   return (
     <div className="App-container">
       <div className="App">
+        <img className="Pfp" src={Pfp}/>
         <div className="Slogan">
           <h1 className="Name">JAY SEBAGH</h1>
           <p className="Code">&lt;<span className="Div-color">div </span><span className="Class-color">Class</span>="software-engineer"&gt;</p>
-          <p className="Code">&lt;<span className="Div-color">p</span>&gt;Full-Stack Web & Computer Science Software Engineer&lt;<span className="Div-color">&frasl;p</span>&gt;</p>
+          <p className="Code">&lt;<span className="Div-color">p</span>&gt;Full-Stack Web & CS Software Engineer&lt;<span className="Div-color">&frasl;p</span>&gt;</p>
           <p className="Code">&lt;<span className="Div-color">&frasl;div</span>&gt;</p>
         </div>
         <div className="Info-container">
