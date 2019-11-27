85f9330084456dc4879c8d564b6ed8367f459511
JaySebagh <jaysebagh@gmail.com>
2019-11-27 15:19:33 -0800

    Added slogan and updated bg

--- src/App.js
+++ src/App.js
@@ -5,7 +5,12 @@ function App() {
   return (
     <div className="App-container">
       <div className="App">
-        <h1 className="Name">JAY SEBAGH</h1>
+        <div className="Slogan">
+          <h1 className="Name">JAY SEBAGH</h1>
+          <p className="Code">&lt;<span className="Div-color">div </span><span className="Class-color">Class</span>="software-engineer"&gt;</p>
+          <p className="Code">&lt;<span className="Div-color">p</span>&gt;Full-Stack Web & Computer Science Software Engineer&lt;<span className="Div-color">&frasl;p</span>&gt;</p>
+          <p className="Code">&lt;<span className="Div-color">&frasl;div</span>&gt;</p>
+        </div>
       </div>
     </div>
   );
