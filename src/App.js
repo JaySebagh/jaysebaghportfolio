ccfb38cce790f6a020031feadc5a7863a1a2d41f
JaySebagh <jaysebagh@gmail.com>
2019-11-29 02:16:37 -0800

    Updated app to class, updated favicon and title

--- src/App.js
+++ src/App.js
@@ -82,38 +82,43 @@ const year = () => {
   return new Date().getFullYear()
 }
 
-function App() {
-  return (
-    <ModalProvider>
-      <div className="app-container">
-        <div className="app">
-          <img className="pfp" alt="Profile Pic" src={Pfp}/>
-          <div className="slogan">
-            <h1 className="name">JEAN-FRANCOIS SEBAGH</h1>
-            <p className="code">&lt;<span className="div-color">div </span><span className="class-color">class</span>="software-engineer"&gt;</p>
-            <p className="code">&lt;<span className="div-color">p</span>&gt;Full-Stack Web & CS Software Engineer&lt;<span className="div-color">&frasl;p</span>&gt;</p>
-            <p className="code">&lt;<span className="div-color">&frasl;div</span>&gt;</p>
+class App extends React.Component {
+  componentDidMount = () => {
+    document.title = 'Sebagh\'s Portfolio';
+  }
+  render(){
+    return (
+      <ModalProvider>
+        <div className="app-container">
+          <div className="app">
+            <img className="pfp" alt="Profile Pic" src={Pfp}/>
+            <div className="slogan">
+              <h1 className="name">JEAN-FRANCOIS SEBAGH</h1>
+              <p className="code">&lt;<span className="div-color">div </span><span className="class-color">class</span>="software-engineer"&gt;</p>
+              <p className="code">&lt;<span className="div-color">p</span>&gt;Full-Stack Web & CS Software Engineer&lt;<span className="div-color">&frasl;p</span>&gt;</p>
+              <p className="code">&lt;<span className="div-color">&frasl;div</span>&gt;</p>
+            </div>
+            <div className="modal-container">
+              <ModalRoot />
+              <ModalConsumer>
+                {({ showModal }) => (
+                  <Fragment>
+                    <ul className="info-ul">
+                      <li className="info-li" onClick={() => showModal(Intro)}>Intro</li>
+                      <li className="info-li" onClick={() => showModal(Skills)}>Skills</li>
+                      <li className="info-li" onClick={() => showModal(Projects)}>Projects</li>
+                      <li className="info-li" onClick={() => showModal(Contact)}>Contact</li>
+                    </ul>
+                  </Fragment>
+                )}
+              </ModalConsumer>
+            </div>
+            <p className="footer">&copy;{year()} JEAN-FRANCOIS SEBAGH</p>
           </div>
-          <div className="modal-container">
-            <ModalRoot />
-            <ModalConsumer>
-              {({ showModal }) => (
-                <Fragment>
-                  <ul className="info-ul">
-                    <li className="info-li" onClick={() => showModal(Intro)}>Intro</li>
-                    <li className="info-li" onClick={() => showModal(Skills)}>Skills</li>
-                    <li className="info-li" onClick={() => showModal(Projects)}>Projects</li>
-                    <li className="info-li" onClick={() => showModal(Contact)}>Contact</li>
-                  </ul>
-                </Fragment>
-              )}
-            </ModalConsumer>
-          </div>
-          <p className="footer">&copy;{year()} JEAN-FRANCOIS SEBAGH</p>
         </div>
-      </div>
-    </ModalProvider>
-  );
+      </ModalProvider>
+    );
+  }
 }
 
 export default App;
