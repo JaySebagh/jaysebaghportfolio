3ffeb7709bf9251c42c8e4d40c5c9fa85cf0387c
JaySebagh <jaysebagh@gmail.com>
2019-12-02 14:24:13 -0800

    Updated cursor to pointer for list items as well as made name/pfp into links

--- src/App.js
+++ src/App.js
@@ -93,9 +93,9 @@ class App extends React.Component {
       <ModalProvider>
         <div className="app-container">
           <div className="app">
-            <img className="pfp" alt="Profile Pic" src={Pfp}/>
+            <a rel="noopener noreferrer" className="link-contact" href="https://linkedin.com/in/jean-francois-sebagh/" target="_blank"><img className="pfp" alt="Profile Pic" src={Pfp}/></a>
             <div className="slogan">
-              <h1 className="name">JEAN-FRANCOIS SEBAGH</h1>
+              <a rel="noopener noreferrer" className="link-contact" href={"https://drive.google.com/file/d/1mQVWU_eqmBXeopVZ8EUZGYwOMFyT4K2Y/view"} target="_blank"><h1 className="name">JEAN-FRANCOIS SEBAGH</h1></a>
               <p className="code">&lt;<span className="div-color">div </span><span className="class-color">class</span>="software-engineer"&gt;</p>
               <p className="code">&lt;<span className="div-color">p</span>&gt;Full-Stack Web & CS Software Engineer&lt;<span className="div-color">&frasl;p</span>&gt;</p>
               <p className="code">&lt;<span className="div-color">&frasl;div</span>&gt;</p>
