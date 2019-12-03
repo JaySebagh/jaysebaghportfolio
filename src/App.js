10c1bdcf017f122234af7b9269b96a04793f12d5
JaySebagh <jaysebagh@gmail.com>
2019-12-03 11:31:57 -0800

    fixed mobile styling

--- src/App.js
+++ src/App.js
@@ -12,69 +12,77 @@ import projectsData from './projectsData';
 
 const Intro = ({ onRequestClose, ...otherProps }) => (
   <Modal style={{overlay: {backgroundColor: 'none'}}} isOpen onRequestClose={onRequestClose} {...otherProps} className="modal-style">
-    <button onClick={onRequestClose} className="modal-close">&times;</button><p className="title">INTRO</p>
-    <div>
-      <img src={IntroImg} alt="Intro" className="responsive-image"/>
-      <p>Full-Stack Software Engineer who is passionate about creating applications using the latest technologies, learning new things, and expanding knowledge about coding.</p>
+    <div className="mobile-padding">
+      <button onClick={onRequestClose} className="modal-close">&times;</button><p className="title">INTRO</p>
+      <div>
+        <img src={IntroImg} alt="Intro" className="responsive-image"/>
+        <p>Full-Stack Software Engineer who is passionate about creating applications using the latest technologies, learning new things, and expanding knowledge about coding.</p>
 
-      <p>Seeking work opportunities with a knowledgeable, friendly, and collaborative team environment.</p>
+        <p>Seeking work opportunities with a knowledgeable, friendly, and collaborative team environment.</p>
+      </div>
     </div>
   </Modal>
 );
 
 const Skills = ({ onRequestClose, foo, ...otherProps }) => (
   <Modal style={{overlay: {backgroundColor: 'none'}}} isOpen onRequestClose={onRequestClose} {...otherProps} className="modal-style">
-    <button onClick={onRequestClose} className="modal-close">&times;</button><p className="title">SKILLS</p>
-    <div>
-      <img src={SkillsImg} alt="Skills" className="responsive-image"/>
-      <p>Languages:</p>
-      <ul>
-        <li>JavaScript</li>
-        <li>HTML</li>
-        <li>CSS</li>
-        <li>SQL</li>
-        <li>Python</li>
-        <li>Django</li>
-      </ul>
+    <div className="mobile-padding">
+      <button onClick={onRequestClose} className="modal-close">&times;</button><p className="title">SKILLS</p>
+      <div>
+        <img src={SkillsImg} alt="Skills" className="responsive-image"/>
+        <p>Languages:</p>
+        <ul>
+          <li>JavaScript</li>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>SQL</li>
+          <li>Python</li>
+          <li>Django</li>
+        </ul>
 
-      <p>Frameworks/Other:</p>
-      <ul>
-        <li>React</li>
-        <li>Redux</li>
-        <li>Hooks</li>
-        <li>Sass</li>
-        <li>Node.js</li>
-        <li>Jest</li>
-        <li>Heroku</li>
-        <li>Netlify</li>
-        <li>Next.js</li>
-      </ul>
+        <p>Frameworks/Other:</p>
+        <ul>
+          <li>React</li>
+          <li>Redux</li>
+          <li>Hooks</li>
+          <li>Sass</li>
+          <li>Node.js</li>
+          <li>Jest</li>
+          <li>Heroku</li>
+          <li>Netlify</li>
+          <li>Next.js</li>
+        </ul>
+      </div>
     </div>
   </Modal>
 );
 
 const Projects = ({ onRequestClose, foo, ...otherProps }) => (
   <Modal style={{overlay: {backgroundColor: 'none'}}} isOpen onRequestClose={onRequestClose} {...otherProps} className="modal-style">
-    <button onClick={onRequestClose} className="modal-close">&times;</button><p className="title">PROJECTS</p>
-    <div>
-      <img src={ProjectsImg} alt="Projects" className="responsive-image"/>
-      {projectsData.map(data => {return <ProjectCard data={data} />})}
+    <div className="mobile-padding">
+      <button onClick={onRequestClose} className="modal-close">&times;</button><p className="title">PROJECTS</p>
+      <div>
+        <img src={ProjectsImg} alt="Projects" className="responsive-image"/>
+        {projectsData.map(data => {return <ProjectCard data={data} />})}
+      </div>
     </div>
   </Modal>
 );
 
 const Contact = ({ onRequestClose, foo, ...otherProps }) => (
   <Modal style={{overlay: {backgroundColor: 'none'}}} isOpen onRequestClose={onRequestClose} {...otherProps} className="modal-style msc">
-    <button onClick={onRequestClose} className="modal-close">&times;</button>
-    <div className="responsive-center">
-      <p className="title center-contact contact-margin">CONTACT</p>
-      <div className="center-contact">
-        <ul className="contact li-no-indent">
-          <li><a rel="noopener noreferrer" href="https://github.com/jaysebagh/" target="_blank" className="contact-info"><i class="fab fa-github"></i> GITHUB</a></li>
-          <li><a rel="noopener noreferrer" href="https://linkedin.com/in/jean-francois-sebagh/" target="_blank" className="contact-info"><i class="fab fa-linkedin"></i> LINKEDIN</a></li>
-          <li><a rel="noopener noreferrer" href="https://drive.google.com/file/d/1mQVWU_eqmBXeopVZ8EUZGYwOMFyT4K2Y/view" target="_blank" className="contact-info"><i class="far fa-file"></i> RESUME</a></li>
-          <li><span className="contact-info"><i class="far fa-envelope"></i> EMAIL: </span>jaysebagh@gmail.com</li>
-        </ul>
+    <div className="mobile-padding">
+      <button onClick={onRequestClose} className="modal-close">&times;</button>
+      <div className="responsive-center">
+        <p className="title center-contact contact-margin">CONTACT</p>
+        <div className="center-contact">
+          <ul className="contact li-no-indent">
+            <li><a rel="noopener noreferrer" href="https://github.com/jaysebagh/" target="_blank" className="contact-info"><i class="fab fa-github"></i> GITHUB</a></li>
+            <li><a rel="noopener noreferrer" href="https://linkedin.com/in/jean-francois-sebagh/" target="_blank" className="contact-info"><i class="fab fa-linkedin"></i> LINKEDIN</a></li>
+            <li><a rel="noopener noreferrer" href="https://drive.google.com/file/d/1mQVWU_eqmBXeopVZ8EUZGYwOMFyT4K2Y/view" target="_blank" className="contact-info"><i class="far fa-file"></i> RESUME</a></li>
+            <li><span className="contact-info"><i class="far fa-envelope"></i> EMAIL: </span>jaysebagh@gmail.com</li>
+          </ul>
+        </div>
       </div>
     </div>
   </Modal>
