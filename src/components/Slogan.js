cf0753b45e2da9a7eb0941560b684ff2d30c51db
JaySebagh <jaysebagh@gmail.com>
2019-12-03 18:54:49 -0800

    Bug fixed, issue related to Netlify

--- src/components/Slogan.js
+++ src/components/Slogan.js
@@ -7,7 +7,7 @@ const Slogan = () => {
         <div className="slogan-align">
             <a rel="noopener noreferrer" className="link-contact" href="https://linkedin.com/in/jean-francois-sebagh/" target="_blank"><img className="pfp" alt="Profile Pic" src={Pfp}/></a>
             <div className="slogan">
-                <a rel="noopener noreferrer" className="link-contact" href={"https://drive.google.com/file/d/1mQVWU_eqmBXeopVZ8EUZGYwOMFyT4K2Y/view"} target="_blank"><h1 className="name">JEAN-FRANCOIS SEBAGH TEST</h1></a>
+                <a rel="noopener noreferrer" className="link-contact" href={"https://drive.google.com/file/d/1mQVWU_eqmBXeopVZ8EUZGYwOMFyT4K2Y/view"} target="_blank"><h1 className="name">JEAN-FRANCOIS SEBAGH</h1></a>
                 <p className="code">&lt;<span className="div-color">div </span><span className="class-color">class</span>="software-engineer"&gt;</p>
                 <p className="code">&lt;<span className="div-color">p</span>&gt;Full-Stack Web & CS Software Engineer&lt;<span className="div-color">&frasl;p</span>&gt;</p>
                 <p className="code">&lt;<span className="div-color">&frasl;div</span>&gt;</p>
