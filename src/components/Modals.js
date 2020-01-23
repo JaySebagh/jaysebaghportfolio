bbf931e9fcd9e4f6890910a79d7ab6d0bf6a57e7
Jay Sebagh <46274307+JaySebagh@users.noreply.github.com>
2020-01-23 10:21:38 -0800

    Update Modals.js
    Updated Resume link

--- src/components/Modals.js
+++ src/components/Modals.js
@@ -78,7 +78,7 @@ const Intro = ({ onRequestClose, ...otherProps }) => (
             <ul className="contact li-no-indent">
               <li><a rel="noopener noreferrer" href="https://github.com/jaysebagh/" target="_blank" className="contact-info"><i class="fab fa-github"></i> GITHUB</a></li>
               <li><a rel="noopener noreferrer" href="https://linkedin.com/in/jean-francois-sebagh/" target="_blank" className="contact-info"><i class="fab fa-linkedin"></i> LINKEDIN</a></li>
-              <li><a rel="noopener noreferrer" href="https://drive.google.com/file/d/1mQVWU_eqmBXeopVZ8EUZGYwOMFyT4K2Y/view" target="_blank" className="contact-info"><i class="far fa-file"></i> RESUME</a></li>
+              <li><a rel="noopener noreferrer" href="https://drive.google.com/file/d/17qmJR20ogh2worZG-84FceA85I0JeXlp/view?usp=sharing" target="_blank" className="contact-info"><i class="far fa-file"></i> RESUME</a></li>
               <li><span className="contact-info"><i class="far fa-envelope"></i> EMAIL: </span>jaysebagh@gmail.com</li>
             </ul>
           </div>
