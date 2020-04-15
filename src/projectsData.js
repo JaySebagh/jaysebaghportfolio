42c9653a9c37c45ed2c97ae5929e691138ac1e46
Jay Sebagh <46274307+JaySebagh@users.noreply.github.com>
2020-04-15 14:11:34 -0700

    updated URLS to functional demos

--- src/projectsData.js
+++ src/projectsData.js
@@ -3,7 +3,7 @@ const projectsData = [
         project: 1,
         name: "Spotifinder",
         repo: "https://github.com/bw-spotify/frontend",
-        demo: "http://www.spotifinder.ml/",
+        demo: "https://bw-spotify.netlify.app/",
         intro: "A dashboard application to enable users to browse and visualize audio features of over 116k Spotify songs.",
         bullets: [
             "Worked on a team of 8, including collaborating with Data Scientists.",
@@ -17,7 +17,7 @@ const projectsData = [
         project: 2,
         name: "Well-Done",
         repo: "https://github.com/labs15-well-done/frontend",
-        demo: "https://frontend.welldone.now.sh/",
+        demo: "https://frontend-git-master.welldone.now.sh/",
         intro: "A dashboard application to allow an NPO and its clients to monitor current water pump statuses in Cambodia.",
         bullets: [
             "Built a server side rendered React application using Next.js",
@@ -31,7 +31,7 @@ const projectsData = [
         project: 3,
         name: "Hextech GG (in progress)",
         repo: "https://github.com/League-Stats",
-        demo: "",
+        demo: "https://hextech.gg/",
         intro: "A comprehensive statistical and match history website for League of Legends players.",
         bullets: [
             "Interacted with Riot Game's official developer API",
